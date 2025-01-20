c80f3d815a24c74012f2aa48631a09e0245f4e22
Andre van Zuydam <andrevanzuydam@gmail.com>
2025-01-20 12:41:36 +0200

    Initial commits for MYSQL database

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,14 @@
 # Start your project here
+import os
 
 from tina4_python.Database import Database
 from tina4_python.Migration import migrate
 
-dba = Database("sqlite3:combine.db")
+database_path = os.getenv("DATABASE_PATH", "db-mysql-nyc3-mentalmetrix-do-user-4490318-0.c.db.ondigitalocean.com/25060:qfinder")
+
+dba = Database(f"mysql.connector:{database_path}",
+               os.getenv("DATABASE_USERNAME", "doadmin"),
+               os.getenv("DATABASE_PASSWORD", "doadmin"))
 migrate(dba)
 
 from routes import *
