aa7a19763c11c5d0c42f80c3bbdd5318835dd13b
Andre van Zuydam <andrevanzuydam@gmail.com>
2025-01-20 19:31:10 +0200

    Added test for queue

--- src/routes/home.py
+++ src/routes/home.py
@@ -1,6 +1,24 @@
+import os
 from tina4_python.Router import get
+from tina4_python.Queue import Queue, Config, Producer
+
+config = Config()
+config.queue_type = 'rabbitmq'
+config.prefix = ""
+config.rabbitmq_config = {
+    'host': os.getenv('RABBITMQ_SERVER'),
+    'port': 5672,
+    'username': os.getenv('RABBITMQ_USERNAME'),
+    'password': os.getenv('RABBITMQ_PASSWORD'),
+}
+
+queue = Queue(config, topic="server-start")
+
+producer = Producer(queue)
 
 @get("/")
 async def index(request, response):
     request.session.set("logged_in", True)
+
+    producer.produce({"event": "Hitting route"})
     return response("Ok")
