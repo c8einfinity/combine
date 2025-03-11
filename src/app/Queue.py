feab962c8d4a982adaa6d7161e3868ca1507b609
Jacques van Zuydam <jtvanzuydam@gmail.com>
2025-03-11 17:11:43 +0200

    fix: transcription counts on dashboard

--- src/app/Queue.py
+++ src/app/Queue.py
@@ -7,15 +7,21 @@ def get_total_transcribed_stats():
     from ..orm.Queue import Queue
 
     queue = Queue()
-    unprocessed_queue = queue.__dba__.fetch("select count(*) as unprocessed_count from queue where processed = 0 and action = 'transcribe'")
+    unprocessed_queue = queue.__dba__.fetch_one("select count(*) as unprocessed_count from queue where processed = 0 and action = 'transcribe'")
 
-    processed_queue = queue.__dba__.fetch("select count(*) as processed_count from queue where processed = 1 and action = 'transcribe'")
+    processed_queue = queue.__dba__.fetch_one("select count(*) as processed_count from queue where processed = 1 and action = 'transcribe'")
 
-    total_media = queue.__dba__.fetch("select count(*) as total_media from player_media where media_type = 'video-youtube' and is_deleted = 0")
+    total_media = queue.__dba__.fetch_one("select count(*) as total_media_count from player_media where media_type = 'video-youtube' and is_deleted = 0 and is_valid = 1")
 
-    valid_media = queue.__dba__.fetch("select count(*) as valid_media from player_media where media_type = 'video-youtube' and is_deleted = 0 and is_valid = 1")
+    valid_media = queue.__dba__.fetch_one("select count(*) as valid_media_count from player_media where media_type = 'video-youtube' and is_deleted = 0 and is_valid = 1 and is_sorted = 1")
 
-    return {"unprocessed": unprocessed_queue[0], "processed": processed_queue[0], "total_media": total_media[0], "valid_media": valid_media[0]}
+    return {
+        "unprocessed": unprocessed_queue['unprocessed_count'],
+        "processed": processed_queue['processed_count'],
+        "total_transcribed": unprocessed_queue['unprocessed_count'] + processed_queue['processed_count'],
+        "total_media": total_media['total_media_count'],
+        "valid_media": valid_media['valid_media_count']
+    }
 
 def get_player_transcribed_stats(player_id):
     """
