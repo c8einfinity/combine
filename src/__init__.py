9ddfb354edbac945fec90749636aba9a6a4ec20f
Jacques van Zuydam <jtvanzuydam@gmail.com>
2025-05-20 12:39:42 +0200

    fix(Qfinder): Missing sport fetch in init script

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from tina4_python.Database import Database
 from tina4_python.Migration import migrate
 from tina4_python.ORM import orm
 
-from .app.Setup import check_setup, check_players
+from .app.Setup import check_setup, check_players, sync_sports_positions
 global dba
 
 database_path = os.getenv("DATABASE_PATH", "db-mysql-nyc3-mentalmetrix-do-user-4490318-0.c.db.ondigitalocean.com/25060:qfinder")
@@ -17,7 +17,10 @@ orm(dba)
 
 check_setup(dba)
 
+if os.getenv("TEAMQ_REFRESH_SPORTS", "TRUE") == "TRUE":
+    # sync sports positions
+    sync_sports_positions(dba)
+
 # import everything
 from .app import Setup, Utility, Scraper, QueueUtility
 from .routes import athletes, dashboard, home, users, user_groups, websocket, queue
-
