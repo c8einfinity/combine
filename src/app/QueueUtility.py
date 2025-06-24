253d12e5c215bc49b20045f7f4245eeff5b2538e
Jacques van Zuydam <jtvanzuydam@gmail.com>
2025-06-24 14:00:10 +0200

    fix(QFinder): Queue closing off item

--- src/app/QueueUtility.py
+++ src/app/QueueUtility.py
@@ -207,6 +207,7 @@ def process_item(queue, err, msg):
 
     queue_result = Queue(config, topic="result")
     Producer(queue_result).produce({"processed": True, "message_id": msg.message_id, "message": "OK"})
+    queue.done(msg.message_id)
     return None
 
 
